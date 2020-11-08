fd89fc67ac964ece2575a3937d317b291a5c8665
edave <edave@adagency.com.au>
2020-11-09 09:05:21 +1100

    Use to run powershell script with Admin Rights

--- Add_Windows10_VPN.bat
+++ Add_Windows10_VPN.bat
@@ -1,4 +1,4 @@
-rem PowerShell.exe -ExecutionPolicy UnRestricted -File "%~dpn0.ps1"
-rem To keep script window open for debugging, add the argument -NoExit before -File in the command below
+@rem PowerShell.exe -ExecutionPolicy UnRestricted -File "%~dpn0.ps1"
+@rem To keep script window open for debugging, add the argument -NoExit before -File in the command below
 Powershell.exe -Command "& {Start-Process Powershell.exe -ArgumentList '-ExecutionPolicy UnRestricted -File %~dpn0.ps1' -Verb RunAs}"
 exit
